594f16c175ee64a663c10090d0f0c921d96a47bb
Hariom Rathore <hariomrathore174@gmail.com>
2026-03-27 23:58:11 +0530

    New changes

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-import { cloudflare } from "@cloudflare/vite-plugin";
-
-export default defineConfig({
-  base: '/my-portfolio/',
-  plugins: [react(), cloudflare()],
-})
